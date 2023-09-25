b5aea5a9191f6fe2321ce1bf6b982efb2cd01d2a
Heidi Fryzell <hfryzell@gmail.com>
2023-09-25 14:45:49 -0400

    basic CSS styling applied to movie

--- app.js
+++ app.js
@@ -5,20 +5,21 @@ fetch("http://www.omdbapi.com/?i=tt3896198&apikey=80d36601&t=Blade+Runner")
     .then(function (data) {
         console.log(data)
         movieResultDiv.innerHTML += `
-        <div>
-            <div>
-            <img src="${data.Poster}" id="poster" />
-            <div>
-            <div>
-                <div>
+        <div id="movie-div">
+            <div id="poster-div">
+                <img src="${data.Poster}" id="poster" />
+            </div>
+            <div id="text-div">
+                <div id="title-div">
                     <h2>${data.Title}</h2>
-                    <p>${data.imdbRating}</p>
+                    <p>⭐️ ${data.imdbRating}</p>
                 </div>
-                <div>
+                <div id="details-div">
                     <p>${data.Runtime}</p>
                     <p>${data.Genre}</p>
-                    <button>+ Watchlist</button>
-                <div>
+                    <button><span id="circle-button">+</span> Watchlist</button>
+                </div>
+                <div id="plot-div">
                     <p>${data.Plot}</p>
                 <div>
                 </div>
