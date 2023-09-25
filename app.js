ea8ff7ec0317f5afde738cf59937197390b76ace
Heidi Fryzell <hfryzell@gmail.com>
2023-09-25 15:08:09 -0400

    returning movie from db on search

--- app.js
+++ app.js
@@ -1,30 +1,36 @@
 const movieResultDiv = document.getElementById("movie-result")
+const searchForm = document.getElementById("search-form")
+const searchValue = document.getElementById("movie-search");
 
-fetch("http://www.omdbapi.com/?i=tt3896198&apikey=80d36601&t=Blade+Runner")
-    .then(response => response.json())
-    .then(function (data) {
-        console.log(data)
-        movieResultDiv.innerHTML += `
-        <div id="movie-div">
-            <div id="poster-div">
-                <img src="${data.Poster}" id="poster" />
-            </div>
-            <div id="text-div">
-                <div id="title-div">
-                    <h2>${data.Title}</h2>
-                    <p>⭐️ ${data.imdbRating}</p>
+searchForm.addEventListener("submit", function(event){
+    event.preventDefault();
+    console.log(searchValue.value)
+    fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=80d36601&t=${searchValue.value}`)
+        .then(response => response.json())
+        .then(function (data) {
+            console.log(data)
+            movieResultDiv.innerHTML += `
+            <div id="movie-div">
+                <div id="poster-div">
+                    <img src="${data.Poster}" id="poster" />
                 </div>
-                <div id="details-div">
-                    <p>${data.Runtime}</p>
-                    <p>${data.Genre}</p>
-                    <button><span id="circle-button">+</span> Watchlist</button>
-                </div>
-                <div id="plot-div">
-                    <p>${data.Plot}</p>
+                <div id="text-div">
+                    <div id="title-div">
+                        <h2>${data.Title}</h2>
+                        <p>⭐️ ${data.imdbRating}</p>
+                    </div>
+                    <div id="details-div">
+                        <p>${data.Runtime}</p>
+                        <p>${data.Genre}</p>
+                        <button><span id="circle-button">+</span> Watchlist</button>
+                    </div>
+                    <div id="plot-div">
+                        <p>${data.Plot}</p>
+                    <div>
+                    </div>
                 <div>
-                </div>
-            <div>
-        </div>
-        `
-    }
-    )
+            </div>
+            `
+        }
+        )
+})
